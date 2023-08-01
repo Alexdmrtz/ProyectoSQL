1e93a1ade451bc73f2280725f529b9c13e0f1484
Alexis Martinez <AlexMartinez_156@hotmail.com>
2023-08-01 18:00:24 -0300

    Agregue 3 vistas mas a mi proyecto

--- vistas.sql
+++ vistas.sql
@@ -34,3 +34,40 @@ SELECT s.id_siniestro, s.fecha, s.descripcion,
 FROM Siniestros s
 LEFT JOIN Polizas p ON s.poliza_id = p.id_poliza
 LEFT JOIN Clientes c ON p.cliente_id = c.id_cliente;
+
+--Vista de Pagos con detalles de la Póliza
+
+--Esta vista mostrará información completa sobre los pagos realizados y los detalles de la póliza asociada a cada pago.
+
+CREATE OR REPLACE VIEW VistaPagosConDetallesPoliza AS
+SELECT pa.id_pago, pa.fecha, pa.monto,
+       po.id_poliza, po.numero_poliza, po.fecha_inicio, po.fecha_termino, po.monto_prima,
+       c.id_cliente, c.nombre AS nombre_cliente, c.apellido AS apellido_cliente
+FROM Pagos pa
+LEFT JOIN Polizas po ON pa.poliza_id = po.id_poliza
+LEFT JOIN Clientes c ON po.cliente_id = c.id_cliente;
+
+--Vista de Agentes con información de sus Pólizas
+
+--Esta vista mostrará información completa sobre los agentes y las pólizas que tienen asignadas.
+
+CREATE OR REPLACE VIEW  VistaAgentesConPolizas AS
+SELECT a.id_agente, a.nombre, a.apellido, a.fecha_nacimiento, a.direccion, a.telefono, a.email,
+       po.id_poliza, po.numero_poliza, po.fecha_inicio, po.fecha_termino, po.monto_prima,
+       c.nombre AS nombre_cliente, c.apellido AS apellido_cliente
+FROM Agentes a
+LEFT JOIN Polizas po ON a.id_agente = po.agente_id
+LEFT JOIN Clientes c ON po.cliente_id = c.id_cliente;
+
+--Vista de Sucursales con información de su Compañía y Agentes asignados
+
+--Esta vista mostrará información completa sobre las sucursales, su compañía asociada y los agentes asignados a cada sucursal.
+
+CREATE OR REPLACE VIEW VistaSucursalesConCompañiaYAgentes AS
+SELECT s.id_sucursal, s.nombre AS nombre_sucursal, s.direccion, s.telefono, s.email,
+       c.id_compañia, c.nombre AS nombre_compañia, c.direccion AS direccion_compañia, c.telefono AS telefono_compañia, c.email AS email_compañia,
+       a.id_agente, a.nombre AS nombre_agente, a.apellido AS apellido_agente, a.fecha_nacimiento AS fecha_nacimiento_agente, a.direccion AS direccion_agente, a.telefono AS telefono_agente, a.email AS email_agente
+FROM Sucursales s
+LEFT JOIN Compañias c ON s.compañia_id = c.id_compañia
+LEFT JOIN Agentes a ON s.id_sucursal = a.sucursal_id;
+
