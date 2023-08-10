6ae4eb50605a437c32dab9d9ae3501cc02635ddc
Alexis Martinez <AlexMartinez_156@hotmail.com>
2023-08-10 17:46:06 -0300

    cree el script para la tabla polizas auditorias

--- triggers.sql
+++ triggers.sql
@@ -19,3 +19,35 @@ END$$
 
 
 DELIMITER ;
+
+
+--Trigger para  calcular automáticamente la fecha de término de una póliza en función de la fecha de inicio
+
+--Este trigger realiza el cálculo automático de la fecha de término de una póliza en función de la fecha de inicio, la duración y la unidad de duración proporcionadas durante la inserción. Luego, registra la acción de inserción en la tabla de auditoría "AuditoriaPolizas".
+
+DELIMITER $$
+
+CREATE TRIGGER BeforeInsertPolizaAudit
+BEFORE INSERT ON Polizas
+FOR EACH ROW
+BEGIN
+    DECLARE fecha_termino DATE;
+    
+    IF NEW.fecha_inicio IS NOT NULL AND NEW.duracion IS NOT NULL AND NEW.unidad_duracion IS NOT NULL THEN
+        IF NEW.unidad_duracion = 'dias' THEN
+            SET fecha_termino = DATE_ADD(NEW.fecha_inicio, INTERVAL NEW.duracion DAY);
+        ELSEIF NEW.unidad_duracion = 'meses' THEN
+            SET fecha_termino = DATE_ADD(NEW.fecha_inicio, INTERVAL NEW.duracion MONTH);
+        ELSEIF NEW.unidad_duracion = 'años' THEN
+            SET fecha_termino = DATE_ADD(NEW.fecha_inicio, INTERVAL NEW.duracion YEAR);
+        END IF;
+        
+        SET NEW.fecha_termino = fecha_termino;
+        
+        INSERT INTO AuditoriaPolizas (id_poliza, accion, fecha_hora, detalles)
+        VALUES (NEW.id_poliza, 'Inserción con Fecha de Término Calculada', NOW(), NULL);
+    END IF;
+END$$
+
+
+DELIMITER ;
